a5a49ad30228e03ca99ffe041481aa9a86d76638
Louis B. Moore <lbmoore@hethcote.com>
2018-04-23 21:29:30 -0600

    Backing out unfortunate merge

--- lib/App/Dapper.pm
+++ lib/App/Dapper.pm
@@ -393,14 +393,14 @@ sub render {
     $stash->set('site', $self->{site});
     
     my $dir = $self->{site}->{'Dapper_libs'};
-#    print Dump $dir;
+    print Dump $dir;
     opendir(DIR, $dir) or die $!;
     while (my $file = readdir(DIR)) {
 # Use a regular expression to ignore files beginning with a period
       next if ($file =~ m/^\./);
-#      print "$dir" . "/" . "$file" . "\n";
+      print "$dir" . "/" . "$file" . "\n";
       my $command = read_file($dir . '/' . $file);
-#     print "$command" . "\n";
+     print "$command" . "\n";
       my $resEval = eval $command;
       unless($resEval) {
         print $@;
@@ -488,7 +488,10 @@ sub render {
     $self->copy(".", $self->{output});
 }
 
-# sub serve - Serve the site locally. Pass in the port number. The port number will be used to serve the site contents from the output directory like this: http://localhost:<port>. Here is an example, using the default port 8000:
+# sub serve - Serve the site locally. Pass in the port number. 
+# The port number will be used to serve the site contents from 
+# the output directory like this: http://localhost:<port>. 
+# Here is an example, using the default port 8000:
 # 
 #    use App::Dapper;
 #
@@ -718,7 +721,9 @@ sub build_inventory {
     push @{$self->{site}->{pages}}, \%page;
 }
 
-# sub copy(sourcedir, outputdir) - Copies files and directories from <sourcedir> to <outputdir> as long as they do not made what is contained in $self->{site}->{ignore}.
+# sub copy(sourcedir, outputdir) - Copies files and directories 
+# from <sourcedir> to <outputdir> as long as they do not made what 
+# is contained in $self->{site}->{ignore}.
 
 sub copy {
     my ($self, $dir, $output) = @_;
